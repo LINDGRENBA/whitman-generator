e21e541152c6ac084a9459717decd5a0da324137
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-10-23 16:52:53 -0700

    Add hooks to Poem component.

--- src/components/Poem.js
+++ src/components/Poem.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PoemButton from './PoemButton';
 
-function Poem() {
+const Poem = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [titles, setTitles] = useState([]);
+  const [poems, setPoems] = useState([]);
+
   const poemBoxStyles = {
     display: 'block',
     marginLeft: 'auto',
@@ -17,18 +22,23 @@ function Poem() {
   const poemStyles = {
     border: '2px solid #FFF',
     textAlign: 'center',
+    marginTop: '2rem'
   }
 
   function buttonClick(){
-    console.log("Hello world");
+    const success = document.querySelector('#successfulCall');
+    success.textContent = "Hello there";
+
+    // fetch()
   }
 
   return (
     <React.Fragment>
       <div style={poemBoxStyles}>
         <PoemButton buttonClick={buttonClick}/>
-        <div id="successfulCall">
-          <h1 style={poemStyles}>Poem Here</h1>
+        <div style={poemStyles}>
+          <h1 id="poemTitle">Poem Here</h1>
+          <p id="successfulCall"></p>
         </div>
         <div id="call-failed">
 
