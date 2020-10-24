da9a405f881a72a91138760ad8ad29a520d96d81
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-10-23 20:05:26 -0700

    Update Poem.js component with branching.

--- src/components/Poem.js
+++ src/components/Poem.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PoemButton from './PoemButton';
 
 const poemBoxStyles = {
@@ -18,11 +18,49 @@ const poemStyles = {
   textAlign: 'center',
   marginTop: '2rem'
 }
+
+
 class Poem extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      poemTitles: []
+    };
+  }
+
+  // add functionality to fetch call to handle various error codes (e.g. 404)
+
+  makeApiCall = () => {
+    fetch("https://pafmon-walt-whitman-poems.p.rapidapi.com/poems/", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "pafmon-walt-whitman-poems.p.rapidapi.com",
+        "x-rapidapi-key": `${process.env.REACT_APP_API_KEY}`
+      }
+    })
+    .then(response => response.json())
+    .then((jsonifiedResponse) => {
+      this.setState({
+        isLoaded: true,
+        poemTitles: jsonifiedResponse[0]
+      });
+      return "dude";
+    })
+    .catch((error) => {
+      this.setState({
+        isLoaded: true,
+        error
+      });
+    });
+  }
 
   buttonClick() {
     const success = document.querySelector('#successfulCall');
-    
+    // let result = this.makeApiCall(); 
+    // console.log(result);
+
     // fetch("https://pafmon-walt-whitman-poems.p.rapidapi.com/poems/", {
     //   "method": "GET",
     //   "headers": {
@@ -40,20 +78,31 @@ class Poem extends React.Component {
   }
   
   render() {
-    return (
-      <React.Fragment>
-        <div style={poemBoxStyles}>
-          <PoemButton buttonClick={this.buttonClick}/>
-          <div style={poemStyles}>
-            <h1 id="poemTitle">Poem Here</h1>
-            <p id="successfulCall"></p>
+    const { error, isLoaded, poemTitles } = this.state;
+    if(error){
+      return <React.Fragment>Error: {error.message}</React.Fragment>
+    } 
+    
+    // else if (!isLoaded) {
+    //   return <React.Fragment>Loading...</React.Fragment>
+    // } 
+    
+    else {
+      return (
+        <React.Fragment>
+          <div style={poemBoxStyles}>
+            <PoemButton buttonClick={this.buttonClick}/>
+            <div style={poemStyles}>
+              <h1 id="poemTitle">Poem Here {poemTitles[0]}</h1>
+              <p id="successfulCall"></p>
+            </div>
+            <div id="call-failed">
+  
+            </div>
           </div>
-          <div id="call-failed">
-
-          </div>
-        </div>
-      </React.Fragment>
-    )
+        </React.Fragment>
+      )
+    }
   }
 
 }
